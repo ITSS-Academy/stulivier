6a56d4c1aefdeba0ddb787faaa68d2e701d2e195
vmtri <tri.vm06567@sinhvien.hoasen.edu.vn>
2025-02-20 11:12:10 +0700

    init project

---
+++ client/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBCacmeYnw-BtvadkEx24Bdy_APxUQSNFY',
+    authDomain: 'stulivier.firebaseapp.com',
+    projectId: 'stulivier',
+    storageBucket: 'stulivier.firebasestorage.app',
+    messagingSenderId: '364273834519',
+    appId: '1:364273834519:web:199536194658e84d8f9a52',
+  },
+  apiUrl: 'http://localhost:3000',
+  socketIoConfig: { url: 'http://localhost:3000', options: {} },
+};
