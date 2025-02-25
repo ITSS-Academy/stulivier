379bbb834a12ff9c58c85aba23f88d0fac92f26a
QuocDuy2211 <DUY.NQ06826@sinhvien.hoasen.edu.vn>
2025-02-25 13:21:27 +0700

    feat: add subscription card component with styling and sample data

--- client/src/app/pages/subscriptions/subscriptions.component.ts
+++ client/src/app/pages/subscriptions/subscriptions.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared/modules/shared.module';
 import { MaterialModule } from '../../../shared/modules/material.module';
 import { VideoModule } from '../../../shared/modules/video.module';
+import {SubscriptionCardComponent} from '../../components/subscription-card/subscription-card.component';
 
 @Component({
   selector: 'app-subscriptions',
   standalone: true,
-  imports: [SharedModule, MaterialModule, VideoModule],
+  imports: [SharedModule, MaterialModule, VideoModule, SubscriptionCardComponent],
   templateUrl: './subscriptions.component.html',
   styleUrl: './subscriptions.component.scss',
 })
