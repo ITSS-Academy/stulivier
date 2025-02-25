2b3b293e4b5b0c3731a52917e57ee9c9f5350ba9
Truong Hoang Phi <phi.th04840@sinhvien.hoasen.edu.vn>
2025-02-25 13:01:30 +0700

    feat: enhance create video and edit profile components with form validation and improved UI

--- client/src/app/dialogs/create-video/create-video.component.ts
+++ client/src/app/dialogs/create-video/create-video.component.ts
@@ -7,6 +7,12 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
 import {ProgressBarMode} from '@angular/material/progress-bar';
 import {MatSlider, MatSliderThumb} from '@angular/material/slider';
+import {ChangeDetectionStrategy, signal} from '@angular/core';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {merge} from 'rxjs';
+import {FormControl, FormGroup, isFormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-create-video',
@@ -34,6 +40,49 @@ export class CreateVideoComponent implements AfterViewInit {
       const el = this.textarea.nativeElement;
       el.style.height = 'auto';
       el.style.height = el.scrollHeight + 'px';
+    }
+  }
+
+  createVideo= new FormGroup({})
+
+  readonly VideoName = new FormControl('', [Validators.required, Validators.maxLength(100)]);
+  readonly Describe = new FormControl('', [Validators.maxLength(1000)]);
+
+  errorVideoNameMessage = signal('');
+  errorDescribeMessage = signal('');
+
+  constructor() {
+    merge(this.Describe.statusChanges, this.Describe.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateDescribeErrorMessage());
+
+    merge(this.VideoName.statusChanges, this.VideoName.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateVideoNameErrorMessage());
+  }
+
+  updateVideoNameErrorMessage() {
+    if (this.VideoName.hasError('required')) {
+      this.errorVideoNameMessage.set('You must enter a value');
+    } else {
+      this.errorVideoNameMessage.set('');
+    }
+  }
+
+  updateDescribeErrorMessage() {
+    if (this.Describe.hasError('required')) {
+      this.errorDescribeMessage.set('You must enter a value');
+    } else {
+      this.errorDescribeMessage.set('');
+    }
+  }
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      // Handle the selected file
+      console.log(file);
     }
   }
 }
