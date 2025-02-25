2529de7f7aa1a25879203c6651836fe69cfc77c5
vmtri <tri.vm06567@sinhvien.hoasen.edu.vn>
2025-02-25 13:48:23 +0700

    feat: add CategoryCardComponent to categories and update home layout

--- client/src/app/pages/categories/categories.component.ts
+++ client/src/app/pages/categories/categories.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared/modules/shared.module';
 import { MaterialModule } from '../../../shared/modules/material.module';
 import { VideoModule } from '../../../shared/modules/video.module';
+import { CategoryCardComponent } from '../../components/category-card/category-card.component';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [SharedModule, MaterialModule, VideoModule],
+  imports: [SharedModule, MaterialModule, VideoModule, CategoryCardComponent],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss',
 })
