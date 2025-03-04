4ea110f3ba702c724dd805f8de3f3f1145edea20
vmtri <tri.vm06567@sinhvien.hoasen.edu.vn>
2025-03-04 16:43:53 +0700

    feat: add category retrieval functionality with detail and top categories support

--- client/src/ngrxs/category/category.actions.ts
+++ client/src/ngrxs/category/category.actions.ts
@@ -1,5 +1,8 @@
 import { createAction, props } from '@ngrx/store';
-import {CategoryModel} from '../../models/category.model';
+import {
+  CategoryDetailModel,
+  CategoryModel,
+} from '../../models/category.model';
 
 export const getAllCategories = createAction('[Category] Get All');
 
@@ -10,5 +13,32 @@ export const getAllCategoriesSuccess = createAction(
 
 export const getAllCategoriesFailure = createAction(
   '[Category] Get All Failure',
+  props<{ error: string }>(),
+);
+
+export const getCategoryById = createAction(
+  '[Category] Get By Id',
+  props<{ id: string }>(),
+);
+
+export const getCategoryByIdSuccess = createAction(
+  '[Category] Get By Id Success',
+  props<{ category: CategoryModel }>(),
+);
+
+export const getCategoryByIdFailure = createAction(
+  '[Category] Get By Id Failure',
+  props<{ error: string }>(),
+);
+
+export const getTopCategories = createAction('[Category] Get Top');
+
+export const getTopCategoriesSuccess = createAction(
+  '[Category] Get Top Success',
+  props<{ topCategories: CategoryDetailModel[] }>(),
+);
+
+export const getTopCategoriesFailure = createAction(
+  '[Category] Get Top Failure',
   props<{ error: string }>(),
 );
